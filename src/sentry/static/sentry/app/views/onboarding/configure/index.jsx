9e4a08a599e69f9ac419418245d8c77e7dd81571
maxbittker <maxbittker@gmail.com>
2017-07-03 18:17:56 -0700

    prject context shim

--- src/sentry/static/sentry/app/views/onboarding/configure/index.jsx
+++ src/sentry/static/sentry/app/views/onboarding/configure/index.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import {onboardingSteps} from '../utils';
 import Waiting from './waiting';
 import ProjectInstallPlatform from '../../projectInstall/platform';
+import ProjectDocsContext from '../../projectInstall/docsContext';
+
+import ProjectContext from '../../projects/projectContext';
 import {platforms} from '../../../../../../integration-docs/_platforms.json';
 
 const Configure = React.createClass({
@@ -11,8 +14,15 @@ const Configure = React.createClass({
     project: React.PropTypes.object
   },
 
+  childContextTypes: {
+    project: React.PropTypes.object
+  },
   getInitialState() {
     return {};
+  },
+
+  getChildContext() {
+    return {project: this.props.project};
   },
 
   steps: Object.keys(onboardingSteps),
@@ -20,6 +30,11 @@ const Configure = React.createClass({
     return (
       <div className="onboarding-Configure">
         <Waiting />
+        <ProjectContext
+          projectId={this.props.project.slug}
+          orgId={this.props.params.orgId}>
+          <ProjectDocsContext />
+        </ProjectContext>
         <ProjectInstallPlatform
           platformData={{
             // TODO(maxbittker) don't hardcode these
@@ -33,6 +48,7 @@ const Configure = React.createClass({
             projectId: this.props.project.slug
           }}
         />
+
         <div className="btn btn-primary" onClick={this.props.next}>
           next step
         </div>
